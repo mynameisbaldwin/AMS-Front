538dafc506a6032a4e4110d66da910b615c20ef4
mynameisbaldwin <corbinedwards@comcast.net>
2019-03-25 01:12:51 -0500

    Initialized Vue with navbar component

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand to="/">Safety Net</b-navbar-brand>
+      <b-navbar-toggle class="ml-auto" id="nav-toggle" target="nav-collapse"/>
+      <b-collapse is-nav id="nav-collapse">
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/about">Account</b-nav-item>
+          <b-nav-item href="#" id="last-nav">Logout</b-nav-item>
+          <b-button id="btn-new-session">New Session</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style>
+</style>
