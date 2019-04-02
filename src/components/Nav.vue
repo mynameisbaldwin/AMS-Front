3a883aed8e5bd3f3e09b8abf2ee91702d26b1d45
mynameisbaldwin <corbinedwards@comcast.net>
2019-04-02 16:58:59 -0500

    Created static home page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="nav">
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand to="/">Safety Net</b-navbar-brand>
+  <div id="nav-top">
+    <b-navbar placement="fixed" toggleable="lg">
+      <b-navbar-brand id="ams-brand" to="/">Safety Net</b-navbar-brand>
       <b-navbar-toggle class="ml-auto" id="nav-toggle" target="nav-collapse"/>
       <b-collapse is-nav id="nav-collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/about">Account</b-nav-item>
-          <b-nav-item href="#" id="last-nav">Logout</b-nav-item>
+          <b-nav-item-dropdown id="last-nav" text="Account" right>
+            <b-dropdown-item to="/login">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
           <b-button id="btn-new-session">New Session</b-button>
         </b-navbar-nav>
       </b-collapse>
@@ -21,4 +22,40 @@ export default {
 </script>
 
 <style>
+#nav-top {
+  background-color: white;
+  color: black;
+  box-shadow: 0px 2px 20px #999999;
+}
+
+#ams-brand {
+  font-family: "Roboto";
+  font-weight: bold;
+}
+
+.nav-item {
+  border-right: 1px dotted grey;
+}
+
+#nav-toggle {
+  margin-left: 2px;
+}
+
+#last-nav {
+  border-right: 0;
+  margin-right: 15px;
+}
+
+#btn-new-session {
+  background-color: inherit;
+  color: black;
+  font-weight: bold;
+  border-color: black;
+  border-radius: 30px;
+}
+
+#btn-new-session:hover {
+  color: white;
+  background-color: black;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,107 @@
 <template>
   <div class="home">
-    <h1>Index</h1>
+    <div class="splash">
+      <h1 id="home-title">Safety Net</h1>
+      <h2 id="home-subtitle">The all-in-one attendance management app</h2>
+      <div id="home-signup">
+        <b-button id="btn-signup">Sign Up</b-button>
+      </div>
+    </div>
+    <div class="home-row">
+      <h2 class="home-row-head">Attendance Made Easy</h2>
+      <div class="home-benefits">
+        <div class="home-benefits-item"></div>
+        <div class="home-benefits-item"></div>
+        <div class="home-benefits-item"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+
+<style>
+.splash {
+  background-color: #0091ff;
+  width: 100%;
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+
+#home-title {
+  text-align: center;
+  font-size: 122pt;
+  font-family: "Open Sans", "sans-serif";
+  font-weight: bold;
+  color: white;
+  letter-spacing: 10px;
+  margin-bottom: 0;
+}
+
+#home-subtitle {
+  text-align: center;
+  font-size: 30pt;
+  font-family: "Roboto", "sans-serif";
+  font-style: italic;
+  font-weight: light;
+  color: white;
+  margin-top: 30px;
+  margin-bottom: 50px;
+}
+
+#home-signup {
+  display: flex;
+  justify-content: center;
+}
+
+#btn-signup {
+  font-size: 30pt;
+  font-family: "Roboto", "sans-serif";
+  background-color: inherit;
+  color: white;
+  border: 3px solid white;
+  border-radius: 10px;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+#btn-signup:hover {
+  background-color: white;
+  color: #0091ff;
+}
+
+.home-row {
+  width: 100%;
+  background-color: white;
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+
+.home-row-head {
+  font-family: "Open Sans", "Roboto", "sans-serif";
+  font-size: 50pt;
+  text-align: center;
+  font-weight: bold;
+}
+
+.home-benefits {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.home-benefits-item {
+  background-color: #d8d8d8;
+  width: 256px;
+  height: 300px;
+  margin-right: 40px;
+}
+</style>
+
